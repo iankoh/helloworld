9e5cadacc0534da10cc4e91f6a920e57fd011c03
Ingyu Koh <ingyukoh@gmail.com>
2016-07-26 16:09:55 -0700

    changes with added hello world 1 2

---
+++ HelloWorld/HelloWorld.cpp
@@ -0,0 +1,15 @@
+// HelloWorld.cpp : 콘솔 응용 프로그램에 대한 진입점을 정의합니다.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	printf("hello world!");
+	printf("hello world1!");
+	printf("hello world2!");
+    return 0;
+}
+
+
